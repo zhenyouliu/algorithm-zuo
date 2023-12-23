f9689cbe6a77442c1db03c5ed0fb3e4e899d132c
刘振友 <zhenyouliu0622@163.com>
2023-12-23 21:00:09 +0800

    快排算法实现

--- src/main/java/com/lzy/class03/Code03_PartitionAndQuickSort.java
+++ src/main/java/com/lzy/class03/Code03_PartitionAndQuickSort.java
@@ -17,8 +17,9 @@ public class Code03_PartitionAndQuickSort {
      * @param arr 输入数组
      * @param L 要执行分区的下标左边界
      * @param R 要执行分区的下标右边界
+     * @return 返回末尾那个元素分区后的下标位置
      */
-    public void partition(int[] arr, int L, int R){
+    public int partition(int[] arr, int L, int R){
 
         int lessEqual = L-1;
         int index = L;
@@ -28,20 +29,27 @@ public class Code03_PartitionAndQuickSort {
             }
             index++;
         }
+        return lessEqual;
     }
 
     /**
      * 对一个非空数组，按照末尾元素，玩荷兰国旗划分
      * @param arr 整数数组
+     * @return index[]：返回等于区的左右边界下标
      */
-    public void netherlandsFlag(int[] arr) {
+    public int[] netherlandsFlag(int[] arr, int L, int R) {
 
-        Assert.isTrue(arr != null && arr.length > 0, "输入数组不允许为空");
-        int less = -1;
+        if(L > R) {
+            return new int[]{-1, -1};
+        }
+        if(L == R) {
+            return new int[]{L, R};
+        }
+        int less = L-1;
         int equal = less;
-        int index = 0;
-        int compVal = arr[arr.length - 1];
-        while(index <= arr.length - 1) {
+        int index = L;
+        int compVal = arr[R];
+        while(index <= R) {
             if(arr[index] < compVal) {
 //                小于比较值，先把等于区右扩，再把小于区右扩
                 swap(arr, less + 1, ++equal);
@@ -52,6 +60,56 @@ public class Code03_PartitionAndQuickSort {
             }
             index++;
         }
+        return new int[]{less+1, equal};
+    }
+
+    /**
+     * 快排算法1：递归地按照末尾元素进行分区。每次递归完成末尾元素的排序。（一次排好一个元素）
+     */
+    public void quickSortByPartition(int[] arr) {
+        if(arr == null || arr.length < 2) {
+            return ;
+        }
+        /**
+         * 执行递归任务：
+         * 1）先通过partition进行一次排序，把末尾元素排好位置。
+         * 2）把子任务分发下去。结束。（子任务回收上来的时候，所有元素就全部排序完成了。）
+         */
+        process1(arr, 0, arr.length - 1);
+    }
+
+    private void process1(int[] arr, int L, int R) {
+        if(L >= R) { // 区间长度小于2，没有必要再排序。
+            return ;
+        }
+        int mid = partition(arr, L, R);
+        process1(arr, L, mid - 1);
+        process1(arr, mid+1, R);
+    }
+
+    /**
+     * 快排算法2：递归地按照末尾元素玩荷兰国旗游戏。每次排好一组与末尾元素相等的元素。
+     */
+    public void quickSortByNetherlands(int[] arr) {
+        if(arr == null || arr.length < 2) {
+            return ;
+        }
+        /**
+         * 执行递归任务：
+         * 1）玩荷兰国旗游戏，把白旗元素确定好
+         * 2）分发子任务。
+         */
+        process2(arr, 0, arr.length - 1);
+
+    }
+
+    void process2(int[] arr, int L, int R) {
+        if(L >= R) {
+            return ;
+        }
+        int[] boundArr = netherlandsFlag(arr, 0, arr.length - 1);
+        process1(arr, L, boundArr[0] - 1);
+        process1(arr, boundArr[1] + 1, R);
     }
 
 }
--- src/test/java/com/lzy/class03/Code03_PartitionAndQuickSortTest.java
+++ src/test/java/com/lzy/class03/Code03_PartitionAndQuickSortTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static com.lzy.utils.AlgoUtil.generateRandomArray;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -38,10 +40,36 @@ class Code03_PartitionAndQuickSortTest {
     @Test
     void testNetherlands() {
         int[] arr = generateRandomArray(20, 100);
-        code03.netherlandsFlag(arr);
+        code03.netherlandsFlag(arr, 0, arr.length - 1);
         log.info("随机数组，按照荷兰国旗分区：{}", arr);
         int[] arrSelf = {1, 3, 2, 5, 5, 6, 7, 0, 8, 5};
-        code03.netherlandsFlag(arrSelf);
+        code03.netherlandsFlag(arrSelf, 0, arrSelf.length - 1);
         log.info("自定义数组，按照荷兰国旗分区后：{}", arrSelf);
+    }
+
+    @Test
+    void testQuickSortByPartition() {
+        for (int i = 0; i < 20; i++) {
+            int[] arr = generateRandomArray(30, 100);
+            code03.quickSortByPartition(arr);
+            log.info("快排后的结果：{}", arr);
+            log.info("-----------------------------");
+        }
+    }
+
+    @Test
+    void testQuickSortByNetherlandsFlag() {
+        for (int i = 0; i < 20; i++) {
+            int[] arr = generateRandomArray(30, 100);
+            int[] arrCopy = Arrays.copyOf(arr, arr.length);
+            code03.quickSortByNetherlands(arr);
+            log.info("快排后的结果：{}", arr);
+            Arrays.sort(arrCopy);
+            if(!Arrays.equals(arr, arrCopy)) {
+                log.error("测试失败");
+                return ;
+            }
+            log.info("-----------------------------");
+        }
     }
 }
